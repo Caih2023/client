8359080a14966f417abe110403c11db33c9cc11b
Caih2023 <estadiacaih@gmail.com>
2023-08-11 14:38:34 -0600

    add files

--- src/pages/noticias/Noticia.jsx
+++ src/pages/noticias/Noticia.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useNoticias } from "../../context/NoticiasContext";
 import NoticiasCard from "../../components/NoticiaCard";
-
+import Navbar from "../../components/Navbar";
+import Footer from "../../context/Footer";
 function Noticia() {
   const { getNoticias, noticias } = useNoticias();
 
@@ -12,10 +13,14 @@ function Noticia() {
   if (noticias.length == 0) return <h1>No hay tareas</h1>;
 
   return (
-    <div className="card-cols">
-      {noticias.map((noticia) => (
-        <NoticiasCard noticia={noticia} key={noticia._id} />
-      ))}
+    <div>
+      <Navbar />
+      <div className="card-cols">
+        {noticias.map((noticia) => (
+          <NoticiasCard noticia={noticia} key={noticia._id} />
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 }

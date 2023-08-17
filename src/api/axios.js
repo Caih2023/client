13406c2865f4fbff65c0c429e9e80285229b5501
Caih2023 <estadiacaih@gmail.com>
2023-08-17 13:17:47 -0600

    add files

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://backend-caih.vercel.app/api",
   // baseURL: "http://localhost:4000/api",
-  // withCredentials: true,
+  withCredentials: true,
 });
 
 export default instance;

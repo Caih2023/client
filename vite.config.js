600c7160afe865b232c9aeca27f7704cc8f25f93
Caih2023 <estadiacaih@gmail.com>
2023-08-18 14:39:09 -0600

    add files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+//esta configuracion es para que funcione en el host de render
 export default defineConfig({
   plugins: [react()],
   server: {
